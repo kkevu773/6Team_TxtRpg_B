0434d3ff6336ac12b65002e9b77c0b79336b8451
1128hsh-maker <1128hsh@gmail.com>
2025-10-14 16:12:33 +0900

    1차 직업 정하기
    1차적으로 직업정하기 일단 업로드

--- 6tTxtRpg/6tTxtRpg/Program.cs
+++ 6tTxtRpg/6tTxtRpg/Program.cs
@@ -26,6 +26,7 @@ namespace _6TxtRpg // 이쪽에 만들기
             this.damage = 10;
             this.defense = 5;
             this.gold = 1500;
+            
         }
         public void YourName() //이름 정하기
         {
@@ -50,6 +51,29 @@ namespace _6TxtRpg // 이쪽에 만들기
                 }
             }
 
+        }
+        public void YourJob() //직업 정하기
+        {
+            while (true)
+            {
+                Console.Write("직업을 선택해주세요(전사, 마법사, 궁수)");
+                string input = Console.ReadLine();
+                job = input;
+                if (job == "전사" || job == "마법사" || job == "궁수")
+                {
+                    Console.WriteLine($"{job}이 맞으십니까?");
+                    string yes = Console.ReadLine();
+                    if (yes == "네" || yes == "예" || yes == "ㅇㅇ" || yes == "ㅇ")
+                    {
+                        Console.WriteLine("확인되었습니다.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("다시 입력해주세요.");
+                    }
+                }
+            }
         }
 
         public void ShowInfo() //정보창
