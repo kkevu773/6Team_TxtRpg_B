4a4016740107aa54f8c4e320f0b60a645538a235
Park ha <115542242+parkha6@users.noreply.github.com>
2025-10-14 16:42:11 +0900

    chore ignore에 스튜디오 코드 디버거 관련 파일 추가
    feat 몬스터 최대 체력 추가

--- 6tTxtRpg/6tTxtRpg/Battle.cs
+++ 6tTxtRpg/6tTxtRpg/Battle.cs
@@ -71,10 +71,10 @@ namespace _6TxtRpg
                 { Console.Write($"{i}."); }
                 if (fightMonsters[i].hp <= 0)//사망시 Dead표시
                 {
-                    //Console.WriteLine($"LV.{fightMonsters[i].level} {fightMonsters[i].name} Hp Dead");//TODO:몬스터 레벨변수 생기면 주석해제
+                    Console.WriteLine($"LV.{fightMonsters[i].level} {fightMonsters[i].name} Hp Dead");//TODO:몬스터 레벨변수 생기면 주석해제
                     Console.ForegroundColor = ConsoleColor.White;
                 }
-                //Console.WriteLine($"LV.{fightMonsters[i].level} {fightMonsters[i].name} Hp {fightMonsters[i].hp}");//배열에 넣은 몬스터 출력
+                Console.WriteLine($"LV.{fightMonsters[i].level} {fightMonsters[i].name} Hp {fightMonsters[i].hp} / {fightMonsters[i].maxHP}");//배열에 넣은 몬스터 출력
             }
         }
         void ShowChar(Character character)//플레이어 정보출력 함수. 가독성을 위해 일단 뺐다.
