2b4c58fe1dea34b772b5e4c05db1300869a41403
Park ha <115542242+parkha6@users.noreply.github.com>
2025-10-15 17:35:15 +0900

    feat 퀘스트 매뉴추가
    style 매뉴 키 개선. 숫자패드도 작동함.

---
+++ 6tTxtRpg/6tTxtRpg/Quest.cs
@@ -0,0 +1,42 @@
+﻿using _6tTxtRpg;
+
+namespace _6tTxtRpg
+{
+    internal class Quest
+    {
+        string[] questName = new string[]
+{
+            "마을을 위협하는 고블린 처치",
+            "장비를 장착해보자",
+            "더욱 더 강해지기!",
+};
+        string[] questInfo = new string[]
+        {
+            @"",
+            @"",
+            @"",
+        };
+        Dictionary<string, (string, int)> QuestDiction;
+        public Quest()
+        {
+            QuestDiction = new Dictionary<string, (string, int)>()
+            {
+                { questName[0],(questInfo[0],10)},
+                { questName[1],(questInfo[1],10)},
+                { questName[2],(questInfo[2],10)},
+            };
+        }
+        public void ShowQuest()
+        {
+            Console.Clear();
+            int i = 1;
+            foreach (var quest in QuestDiction)
+            {
+                Tool.ColorTxt($"{i} ",Tool.color5);
+                Console.WriteLine(quest.Key);
+                ++i;
+            }
+            Console.ReadKey();
+        }
+    }
+}
